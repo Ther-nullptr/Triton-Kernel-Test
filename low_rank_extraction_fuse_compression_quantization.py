chore: add sparse logic, fix bugs of scaling factor

low_rank_extraction_fuse_compression_quantization.py
@@ -4,8 +4,8 @@ import triton.language as tl
 
 @triton.autotune(
     configs=[
-        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 32, 'GROUP_SIZE_M': 4, }, num_stages=2, 
-                      num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 32, 'GROUP_SIZE_M': 4, }, num_stages=1, 
+                      num_warps=2),
     ],
     key=['M', 'N'],
 )
@@ -90,8 +90,8 @@ def low_rank_addition_fuse_compression_quantization_kernel(
     q = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N // elem_per_position), dtype=tl.uint8)
     
     offs_sn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    s_ptrs = s_ptr + stride_sn * offs_sn[None, :] + offs_b * stride_sb
-    s_mask = (offs_b < B) & (offs_sn[None, :] < N)
+    s_ptrs = s_ptr + stride_sn * offs_sn[None, :] 
+    s_mask = (offs_sn[None, :] < N)
     s = tl.load(s_ptrs, mask=s_mask, other=1.0)
     
     x = tl.math.round(x / s)
@@ -146,6 +146,7 @@ def low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit=8, 
         outlier, quantize_bit, elem_per_position,
         BLOCK_SIZE_K=K
     )
+    o = o.to_sparse()
     return o, q
 
 
@@ -166,6 +167,7 @@ def torch_low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_b
         q_new |= (q[:, :, selected_index] << (quantize_bit * i))
     
     q_new = q_new.to(torch.uint8)
+    o = o.to_sparse()
     return o, q_new
 
 
@@ -177,6 +179,6 @@ if __name__ == '__main__':
     r = torch.randn(R, M, device='cuda', dtype=torch.bfloat16)
     x = torch.randn(B, M, M, device='cuda', dtype=torch.bfloat16)
     s = torch.zeros(B, M, device='cuda', dtype=torch.bfloat16) + 1.2
-    o, q = low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit=quantize_bit)
-    o_simple, q_simple = torch_low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit=quantize_bit)
-    print(q.to(torch.int8) - q_simple.to(torch.int8))
+    o, q = low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit=quantize_bit, outlier=5)
+    # o_simple, q_simple = torch_low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit=quantize_bit)
+    # print(q.to(torch.int8) - q_simple.to(torch.int8))

low_rank_extraction_fuse_decompression_dequantization.py
@@ -81,8 +81,8 @@ def low_rank_addition_fuse_decompression_dequantization_kernel(
         
     # dequantize
     offs_sn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    s_ptrs = s_ptr + stride_sn * offs_sn[None, :] + offs_b * stride_sb
-    s_mask = (offs_b < B) & (offs_sn[None, :] < N)
+    s_ptrs = s_ptr + stride_sn * offs_sn[None, :] 
+    s_mask = (offs_sn[None, :] < N)
     s = tl.load(s_ptrs, mask=s_mask, other=1.0)
     
     offs_x_temp_m = offs_xm
@@ -133,6 +133,7 @@ def low_rank_addition_fuse_decompression_dequantization(l, r, q, o, s, quantize_
     )
     x = torch.empty((B, M, N), device=l.device, dtype=torch.bfloat16)
     x_temp = torch.empty((B, M, N), device=l.device, dtype=torch.uint8)
+    o = o.to_dense()
     
     low_rank_addition_fuse_decompression_dequantization_kernel[grid](
         l, r, x, x_temp, o, q, s,
@@ -152,6 +153,7 @@ def low_rank_addition_fuse_decompression_dequantization(l, r, q, o, s, quantize_
 
 
 def torch_low_rank_addition_fuse_decompression_dequantization(l, r, q, o, s, quantize_bit=8):
+    o = o.to_dense()
     element_num = 8 // quantize_bit
     x = torch.empty((q.shape[0], q.shape[1], q.shape[2] * element_num), device=q.device, dtype=torch.int8)
     mask = (1 << quantize_bit) - 1

low_rank_kernel_accuracy_test.py
@@ -17,20 +17,21 @@ def naive_quantization_dequantization(l, r, x, s, quantize_bit=8, outlier=5.):
 
 if __name__ == '__main__':
     M, R = 1024, 16
-    B = 2
+    B = 4
     l = torch.randn(M, R, device='cuda', dtype=torch.bfloat16) 
     r = torch.randn(R, M, device='cuda', dtype=torch.bfloat16) 
-    x = torch.randn(B, M, M, device='cuda', dtype=torch.bfloat16) 
+    x = torch.randn(B, M, M, device='cuda', dtype=torch.bfloat16)
     x_copy = deepcopy(x)
-    s = torch.zeros(B, M, device='cuda', dtype=torch.bfloat16) + 1
-    s[:, :20] = 1.2
-    s[:, 20:] = 1.2
+    s = torch.zeros(1, M, device='cuda', dtype=torch.bfloat16) + 1e-5
+    s[:, 300] = 2e-5
 
     o, q = low_rank_addition_fuse_compression_quantization(l, r, x, s)
     x_decode = low_rank_addition_fuse_decompression_dequantization(l, r, q, o, s)
     x_baseline = naive_quantization_dequantization(l, r, x_copy, s)
     
-    x_diff = x_decode - x_baseline
+    x_diff = (x_decode - x_baseline) / (x_baseline + 1e-5)
+    print(x_decode)
+    print(x_baseline)
     print(x_diff)
-    print(torch.sum(x_diff == 0).item() / x_diff.numel())
+    # print(torch.sum(x_diff == 0).item() / x_diff.numel())
     # print(torch.sum(x_decode - x_baseline))

low_rank_kernel_decode_speed_test.py
@@ -10,7 +10,7 @@ for fp8_inputs in [False]:
     configs.append(
         triton.testing.Benchmark(
             x_names=["M"],  # Argument names to use as an x-axis for the plot
-            x_vals=[256 * i for i in range(2, 100, 10)],  # Different possible values for `x_name`
+            x_vals=[256 * i for i in range(2, 80, 10)],  # Different possible values for `x_name`
             line_arg="provider",  # Argument name whose value corresponds to a different line in the plot
             # Possible values for `line_arg`
             # Don't compare to cublas for fp8 cases as torch.matmul doesn't support fp8 at the moment.
@@ -29,13 +29,15 @@ for fp8_inputs in [False]:
 def benchmark(M, provider, fp8_inputs):
     B = 4
     R = 16
-    quantize_bit = 1
+    quantize_bit = 2
     element_num = 8 // quantize_bit
     x = torch.randn((B, M, M), device="cuda", dtype=torch.bfloat16)
     l = torch.randn((M, R), device="cuda", dtype=torch.bfloat16)
     r = torch.randn((R, M), device="cuda", dtype=torch.bfloat16)
     s = torch.randn((B, M), device="cuda", dtype=torch.bfloat16) + 2
     o = torch.randn((B, M, M), device=x.device, dtype=torch.bfloat16)
+    o = o * (o > 1)
+    o = o.to_sparse()
     q = torch.zeros((B, M, M // element_num), device=x.device, dtype=torch.uint8)
     quantiles = [0.5, 0.2, 0.8]
     if provider == "cublas":

low_rank_kernel_encode_speed_test.py
@@ -10,7 +10,7 @@ for fp8_inputs in [False]:
     configs.append(
         triton.testing.Benchmark(
             x_names=["M"],  # Argument names to use as an x-axis for the plot
-            x_vals=[256 * i for i in range(2, 100, 10)],  # Different possible values for `x_name`
+            x_vals=[256 * i for i in range(2, 80, 10)],  # Different possible values for `x_name`
             line_arg="provider",  # Argument name whose value corresponds to a different line in the plot
             # Possible values for `line_arg`
             # Don't compare to cublas for fp8 cases as torch.matmul doesn't support fp8 at the moment.
@@ -30,6 +30,7 @@ def benchmark(M, provider, fp8_inputs):
     B = 4
     R = 16
     quantize_bit = 1
+    outlier = 12
     x = torch.randn((B, M, M), device="cuda", dtype=torch.bfloat16)
     l = torch.randn((M, R), device="cuda", dtype=torch.bfloat16)
     r = torch.randn((R, M), device="cuda", dtype=torch.bfloat16)
@@ -38,11 +39,11 @@ def benchmark(M, provider, fp8_inputs):
     if provider == "cublas":
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_low_rank_addition(l, r, x), quantiles=quantiles)
     if provider == 'cublas-w-tail':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit), quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit, outlier), quantiles=quantiles)
     if provider == 'triton-w/o-tail':
         ms, min_ms, max_ms = triton.testing.do_bench(lambda: low_rank_addition(l, r, x), quantiles=quantiles)
     if provider == 'triton-w-tail':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit), quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: low_rank_addition_fuse_compression_quantization(l, r, x, s, quantize_bit, outlier), quantiles=quantiles)
     perf = lambda ms: 2 * B * M * R * M * 1e-12 / (ms * 1e-3)
     return perf(ms), perf(max_ms), perf(min_ms)
 